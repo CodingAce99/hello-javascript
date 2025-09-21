7c96dbc825237f5f0db9177eb1a0260ef5cf54e1
VetAdvanceAi <vetadvanceai@gmail.com>
2025-09-21 22:02:35 +0200

    completado ejercicio 09 y 13

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -7,23 +7,120 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=8652
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
 
+    let nombre = "Hector"
+
+    if (nombre == "Hector"){
+        console.log(nombre)
+    } else{
+        console.log("la variable no contiene tu nombre")
+    }
+
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+
+    let user = "admin"
+    let pass = "123"
+
+    if (user === "admin" && pass === "123"){
+        console.log("login valido")
+    } else ("login incorrecto")
 
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 
+    let num = 1
+
+    if (num > 0){
+        console.log("El numero es positivo")
+    } else if (num === 0) {
+        console.log("El numero es cero")
+    } else if (num < 0) {
+        console.log("El numero es negativo")
+    }
+
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+
+    let edad = 15
+
+    if (edad >= 18) {
+        console.log("La persona puede votar")
+    } else {
+        console.log("La persona no puede votar")
+    }
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
 
+    let persona = (edad >= 18) ? "adulto" : "menor"
+
+    console.log(persona)
+
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
 
+    let mes = 9
+
+    if (mes >= 3 && mes <= 6) {
+        console.log("primavera")
+    } else if (mes > 6 && mes <= 9) {
+        console.log("verano")
+    } else if (mes > 9 && mes <= 11) {
+        console.log("otoño")
+    } else {
+        console.log("invierno")
+    }
+
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+
+    if (mes === 2) {
+        console.log("28 o 29 dias si es bisiesto")
+    } else if ([4, 6, 9, 11].includes(mes)) {
+        console.log("30 dias")
+    } else {
+        console.log("31 dias")
+    }
 
 // switch
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+let idioma = "español"   
+
+switch (idioma) {
+    case "español": 
+        console.log("Hola")
+        break
+    case "inglés":
+        console.log("Hello")
+        break
+    case "francés":
+        console.log("Bonjour")
+        break
+    default:
+        console.log("otro idioma")
+}
 
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
 
+    switch (mes) {
+        case 3: case 4: case 5:
+            console.log("primavera")
+            break
+        case 6: case 7: case 8:
+            console.log("verano")
+            break
+        case 9: case 10: case 11:
+            console.log("otoño")
+            break
+        default:
+            console.log("invierno")
+    }
+
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+
+    switch (mes) {
+        case 2:
+            console.log("28 o 29")
+            break
+        case 4: case 6: case 9: case 11:
+            console.log("30 dias")
+            break
+        default:
+            console.log("31 dias")
+    }
--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -5,20 +5,71 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=11451
 
 // 1. Crea un array que almacene cinco animales
 
+    let myArray = ["perro", "gato", "elefante", "leon", "mono"]
+
 // 2. Añade dos más. Uno al principio y otro al final
+
+    myArray.push("tigre")
+    myArray.push("tortuga")
 
 // 3. Elimina el que se encuentra en tercera posición
 
+    myArray.pop(3)
+    console.log(myArray)
+
 // 4. Crea un set que almacene cinco libros
+
+    let mySet = new Set(["libro_1", "libro_2", "libro_3", "libro_4", "libro_5"])
 
 // 5. Añade dos más. Uno de ellos repetido
 
+    mySet.add("libro_6")
+    mySet.add("libro_3")
+
 // 6. Elimina uno concreto a tu elección
+
+    mySet.delete("libro_4")
+    console.log(mySet)
 
 // 7. Crea un mapa que asocie el número del mes a su nombre
 
+    let myMap = new Map([
+        [1, "enero"],
+        [2, "febrero"],
+        [3, "marzo"],
+        [4, "abril"],
+        [5, "mayo"],
+        [6, "junio"],
+        [7, "julio"],
+        [8, "agosto"],
+        [9, "septiembre"],
+        [10, "octubre"],
+        [11, "noviembre"],
+        [12, "diciembre"]
+    ])
+
+    console.log(myMap)
+
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+
+    console.log(myMap.get(5))
 
 // 9. Añade al mapa una clave con un array que almacene los meses de verano
 
+    let arrayVerano = ["junio", "julio", "agosto"]
+    myMap.set("meses_verano", arrayVerano)
+    console.log(myMap)
+
 // 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
+    let nuevoArray = ["Spain", "Italy", "France"]
+    console.log(nuevoArray)
+
+    let nuevoSet = new Set(nuevoArray)
+    console.log(nuevoSet)
+
+    let nuevoMap = new Map([
+        ["nuevoSet", nuevoSet]
+        ])
+
+    console.log(nuevoMap)
+    console.log(nuevoMap.get("nuevoSet"))
